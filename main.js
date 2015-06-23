4daaa93717d126afcd8dfe17f5cce06639fe0f9f
Bryan Duplantis <duplantis@gmail.com>
2015-06-23 12:56:15 -0500

    new changes

--- main.js
+++ main.js
@@ -1,23 +1,68 @@
-var QUOTE_API_URL = 'http://api.theysaidso.com/qod?category=love'
+// var QUOTE_API_URL = 'http://api.theysaidso.com/qod?category=love'
 var FIREBASE_URL = 'https://my-cappstone.firebaseio.com/'
 var loveBtn;
 var truthBtn;
 var beautyBtn;
 
+function getQuotes () {
 
-$.get(FIREBASE_URL + "quotes.json", function (data) {
-  Object.keys(data).forEach(function () {
-    addQuoteDetail(data);
-  });
-});
+};
+
+// Button click code
 
 loveBtn = $(".loveBtn");
-truthBtn = $("truthBtn");
-beautyBtn = $("beautyBtn");
+truthBtn = $(".truthBtn");
+beautyBtn = $(".beautyBtn");
 
-$(".loveBtn").on("click", function (data) {
+$(".loveBtn").on("click", function() {
+  $.get(FIREBASE_URL + "quotes.json", function (data) {
+    console.log(data[0].author);
+    console.log(data[0].quote);
+
+    var quote = data[0].quote;
+    var author = data[0].author;
+
+    $("#quote").html(quote)
+    $("#author").html(author)
+})
+  });
+
+$(".truthBtn").on("click", function() {
+  $.get(FIREBASE_URL + "quotes.json", function (data) {
+    console.log(data[1].author);
+    console.log(data[1].quote);
+
+    var quote = data[1].quote;
+    var author = data[1].author;
+
+    $("#quote").html(quote)
+    $("#author").html(author)
 
 })
+  });
+
+$(".beautyBtn").on("click", function() {
+  $.get(FIREBASE_URL + "quotes.json", function (data) {
+    console.log(data[2].author);
+    console.log(data[2].quote);
+
+    var quote = data[2].quote;
+    var author = data[2].author;
+
+    $("#quote").html(quote)
+    $("#author").html(author)
+
+})
+  });
+
+// function addQuoteDetail(data) {
+
+//   var detail = createMovieNode(data);
+//   var target = $('#quote');
+
+//   target.empty();
+//   target.append(detail);
+// }
 
 
 
@@ -34,3 +79,40 @@ function getJSON(url, cb) {
 
     xhr.send();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TeamTreeHouse code for parsing JSON data
+
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//   if(xhr.readyState === 4) {
+//     var employees = JSON.parse(xhr.responseText);
+//     var statusHTML = '<ul class="bulleted">';
+//     for (var i=0; i<employees.length; i += 1) {
+//       if (employees[i].inoffice === true) {
+//         statusHTML += '<li class="in">';
+//       } else {
+//         statusHTML += '<li class="out">';
+//       }
+//       statusHTML += employees[i].name;
+//       statusHTML += '</li>';
+//     }
+//     statusHTML += '</ul>';
+//     document.getElementById('employeeList').innerHTML = statusHTML;
+//   }
+// };
+// xhr.open('GET', 'data/employees.json');
+// xhr.send();
+
+// end of JSON code
